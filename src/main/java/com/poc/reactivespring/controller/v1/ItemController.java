68db55c9bf7cfd7eb6045454518beba24a7f77f4
carlosssantin <cesantin@gmail.com>
2019-06-04 11:30:05 -0300

    Implemented item delete

--- src/main/java/com/poc/reactivespring/controller/v1/ItemController.java
+++ src/main/java/com/poc/reactivespring/controller/v1/ItemController.java
@@ -34,4 +34,10 @@ public class ItemController {
     public Mono<Item> createItem(@RequestBody Item item) {
         return itemReactiveRepository.save(item);
     }
+
+    @DeleteMapping(ITEM_END_POINT_V1 + "/{id}")
+    public Mono<Void> deleteItem(@PathVariable String id) {
+        return itemReactiveRepository.deleteById(id);
+    }
+
 }
