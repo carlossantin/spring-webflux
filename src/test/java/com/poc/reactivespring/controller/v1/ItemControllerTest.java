5c6a4b819210054fccfe1ea89fad2764e2f2602a
carlosssantin <cesantin@gmail.com>
2019-06-04 10:48:28 -0300

    Created unit tests

--- src/test/java/com/poc/reactivespring/controller/v1/ItemControllerTest.java
+++ src/test/java/com/poc/reactivespring/controller/v1/ItemControllerTest.java
@@ -11,17 +11,22 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @DirtiesContext
 @AutoConfigureWebTestClient
+@ActiveProfiles("test")
 public class ItemControllerTest {
 
     @Autowired
@@ -55,5 +60,35 @@ public class ItemControllerTest {
                 .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
                 .expectBodyList(Item.class)
                 .hasSize(4);
+    }
+
+    @Test
+    public void getAllItems_approach2() {
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Item.class)
+                .hasSize(4)
+                .consumeWith((response) -> {
+                    List<Item> items = response.getResponseBody();
+                    items.forEach((item -> {
+                        assertTrue(item.getId() != null);
+                    }));
+                });
+    }
+
+    @Test
+    public void getAllItems_approach3() {
+        Flux<Item> itemsFlux = webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .returnResult(Item.class)
+                .getResponseBody();
+
+        StepVerifier.create(itemsFlux.log("value from network : "))
+                .expectNextCount(4)
+                .verifyComplete();
     }
 }
