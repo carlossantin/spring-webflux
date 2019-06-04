b1fc9733d966830d8f27cf85d902d3181d971fca
carlosssantin <cesantin@gmail.com>
2019-06-04 10:55:26 -0300

    Implemented get one item

--- src/main/java/com/poc/reactivespring/controller/v1/ItemController.java
+++ src/main/java/com/poc/reactivespring/controller/v1/ItemController.java
@@ -3,9 +3,13 @@ package com.poc.reactivespring.controller.v1;
 import com.poc.reactivespring.document.Item;
 import com.poc.reactivespring.repository.ItemReactiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import static com.poc.reactivespring.constants.ItemConstants.ITEM_END_POINT_V1;
 
@@ -18,5 +22,12 @@ public class ItemController {
     @GetMapping(ITEM_END_POINT_V1)
     public Flux<Item> getAllItems() {
         return itemReactiveRepository.findAll();
+    }
+
+    @GetMapping(ITEM_END_POINT_V1 + "/{id}")
+    public Mono<ResponseEntity<Item>> getOneItem(@PathVariable String id) {
+        return itemReactiveRepository.findById(id)
+                .map(item -> new ResponseEntity<>(item, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
